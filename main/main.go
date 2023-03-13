d1e9ca6ab90119bba6dd158c45147639a1be2822
GuillermoMartinelli <Martinelliguillermo@gmail.com>
2023-03-13 18:49:38 -0300

    first commit

---
+++ main/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	//"net/http"
+)
+
+/* func holaHarlder(w http.ResponseWriter, req *http.Request) {
+	fmt.Fprintf(w,"hola q ashe\n")
+}
+
+func main(){
+	http.HandleFunc("/hola",holaHarlder)
+	http.ListenAndServe(":8080",nil)
+} */
+
